20731d5b67f10c9def117ea546e25973acf130a4
LeandroViegas <43422128+LeandroViegas@users.noreply.github.com>
2019-05-15 02:18:21 -0300

    V 0.14

--- QTallPizzaria/QTallPizzaria/Controllers/EditarPaginasController.cs
+++ QTallPizzaria/QTallPizzaria/Controllers/EditarPaginasController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using QTallPizzaria.Models;
 using System.IO;
+using System.Net;
+using System.Data.Entity;
 
 namespace QTallPizzaria.Controllers
 {
@@ -22,6 +24,32 @@ namespace QTallPizzaria.Controllers
         {
             var banners = db.banner;
             return View(banners.ToList());
+        }
+
+        [HttpPost, ActionName("Banner")]
+        [ValidateAntiForgeryToken]
+        public ActionResult SalvarBanner(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            banner Banner = db.banner.Find(id);
+            if (Banner == null)
+            {
+                return HttpNotFound();
+            }
+            HttpPostedFileBase file = Request.Files["arquivoFoto"];
+            if (file.ContentLength > 0)
+            {
+                string _FileName = (DateTime.Now + Path.GetExtension(file.FileName)).ToString();
+                _FileName = _FileName.Replace("/", "").Replace(":", "").Replace(" ", "");
+                file.SaveAs(Path.Combine(Server.MapPath("~/img/Home/produtos/"), _FileName));
+                Banner.diretorio = "../../img/Home/banners/" + _FileName;
+                db.Entry(Banner).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Banner");
         }
     }
 }
