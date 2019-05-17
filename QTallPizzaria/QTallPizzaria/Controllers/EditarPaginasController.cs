f17e4af188ccc48fe8ef8ab62d0324c3b7db2865
LeandroViegas <l3androviegas@gmail.com>
2019-05-17 15:59:35 -0300

    V 0.17

--- QTallPizzaria/QTallPizzaria/Controllers/EditarPaginasController.cs
+++ QTallPizzaria/QTallPizzaria/Controllers/EditarPaginasController.cs
@@ -7,16 +7,58 @@ using QTallPizzaria.Models;
 using System.IO;
 using System.Net;
 using System.Data.Entity;
+using QTallPizzaria.Classes;
 
 namespace QTallPizzaria.Controllers
 {
     public class EditarPaginasController : Controller
     {
         private DBQtallEntities db = new DBQtallEntities();
+        LimitarString limitar = new LimitarString();
 
         // GET: EditarPaginas
         public ActionResult Index()
         {
+            var evm = new EditarViewModel();
+
+            for (var i = 1; i <= 3; i++)
+                evm.Destaques.Add(new Destaques()
+                {
+                    NomeProduto = db.Produto.Find(db.Destaque.Find(i).idDestaque).nome,
+                    Foto = db.Produto.Find(db.Destaque.Find(i).idDestaque).foto,
+                    preco = db.Produto.Find(db.Destaque.Find(i).idDestaque).preco
+                });
+            foreach (var item in db.Sugestao.ToList())
+            {
+                if (item.idTipo == 1)
+                    evm.Pizza.Add(new Pizza
+                    {
+                        NomeProduto = db.Produto.Find(item.idProduto).nome,
+                        Foto = db.Produto.Find(item.idProduto).foto,
+                        preco = db.Produto.Find(item.idProduto).preco,
+                        Descricao = limitar.Retorno(db.Produto.Find(item.idProduto).descricao, 100)
+                    });
+                if (item.idTipo == 2)
+                    evm.Bebidas.Add(new Bebida
+                    {
+                        NomeProduto = db.Produto.Find(item.idProduto).nome,
+                        Foto = db.Produto.Find(item.idProduto).foto,
+                        preco = db.Produto.Find(item.idProduto).preco,
+                        Descricao = limitar.Retorno(db.Produto.Find(item.idProduto).descricao, 100)
+                    });
+                if (item.idTipo == 3)
+                    evm.Lanches.Add(new Lanche
+                    {
+                        NomeProduto = db.Produto.Find(item.idProduto).nome,
+                        Foto = db.Produto.Find(item.idProduto).foto,
+                        preco = db.Produto.Find(item.idProduto).preco,
+                        Descricao = limitar.Retorno(db.Produto.Find(item.idProduto).descricao, 100)
+                    });
+            }
+
+            ViewBag.idTipoPizzas = new SelectList(db.Produto.Where(p => p.idTipo == 1),"idProduto", "nome");
+            ViewBag.idTipoBebidas = new SelectList(db.Produto.Where(p => p.idTipo == 2),"idProduto", "nome");
+            ViewBag.idTipoLanches = new SelectList(db.Produto.Where(p => p.idTipo == 3),"idProduto", "nome");
             return View();
         }
 
