73ea6a0ec5f31a66b340cd5c3bae163f5c5eeeca
LeandroViegas <L3androviegas@gmail.com>
2019-06-12 22:44:33 -0300

    V 0.1

--- QTallPizzaria/QTallPizzaria/Scripts/Home/Layout.js
+++ QTallPizzaria/QTallPizzaria/Scripts/Home/Layout.js
@@ -5,15 +5,3 @@
     else
         ulSide.style.right = '-300px'
 }
-document.body.onscroll = function () {
-    if (window.pageYOffset > 110) {
-        document.getElementById("nav").style.top = "0px";
-        document.getElementById("nav").style.backgroundColor = "rgb(30,30,30)";
-        document.querySelector(".ulNav").style.padding = "5px 0px 5px 50px";
-    }
-    else {
-        document.getElementById("nav").style.top = "40px";
-        document.getElementById("nav").style.backgroundColor = "rgba(1,1,1,0.5)";
-        document.querySelector(".ulNav").style.padding = "15px 0px 15px 190px";
-    }
-}
